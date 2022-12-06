9e13d81c33bdca0c70e6ad781591f13f0c794d0f
JTomI <36204969+JTomI@users.noreply.github.com>
2022-12-06 17:32:33 -0500

    All pyfiji scripts updated

--- pyfiji/main.py
+++ pyfiji/main.py
@@ -5,6 +5,7 @@ __credits__ = ["Maya Peters-Kostman", "Joseph Incandela"]
 
 import os
 import sys
+import time
 #Find path to pyfiji folder
 scriptpath = sys.argv[0]
 pyfiji_path = os.path.dirname(scriptpath)
@@ -12,12 +13,23 @@ pyfiji_path = os.path.dirname(scriptpath)
 sys.path.append(str(pyfiji_path))
 from pH_weka import WekaPhase
 
-# Set source directory for images
-srcdir = r'F:\Experiments\{}\tifs'.format(exp)
-# Set directory for segmentation output
-outdir = r'F:\Experiments\{}\weka segmentation'.format(exp)
-# Choose which trained segmentation model to use with dataset
-classdir = r'F:\scripts\imagej/weka border segmentation 3.model'
 
+# Set source directory for images
+srcdir = r'C:\Users\jtincan\Desktop\F0312\tifs'
+# Set directory for segmentation output
+outdir = r'C:\Users\jtincan\Desktop\F0312\weka_segmentaion'
+# Choose which trained segmentation model to use with dataset. Models are in Github\Segmentation\weka_models folder.
+classpath = os.path.join(os.path.dirname(pyfiji_path),r'weka_models\Cellasic_BO4F_MPK_12062022.model') 
+
+
+#initialize WekaPhase class
+segmenter = WekaPhase(source_dir=srcdir,output_dir=outdir,classifier_path=classpath)
+filenames = segmenter.filenames
+process = segmenter.process
+run = segmenter.run
 
 #Do analysis here
+
+t0=time.time()
+run()
+print "Time Elapsed: ", time.time()-t0
--- pyfiji/pH_weka.py
+++ pyfiji/pH_weka.py
@@ -5,56 +5,63 @@ __credits__ = ["Maya Peters-Kostman", "Joseph Incandela"]
 
 import os
 import sys
+import time
 from trainableSegmentation import WekaSegmentation, Weka_Segmentation
 from ij import IJ, ImagePlus
-
+from java.util.concurrent import Callable
+from java.util.concurrent import Executors, TimeUnit
 
 class WekaPhase(object):
-	def __init__(self,source_dir=None,output_dir=None,classifier_dir=None):
-		os.chdir('')
+	def __init__(self,source_dir=None,output_dir=None,classifier_path=None):
 		self.srcdir = source_dir
 		self.outdir = output_dir
-		self.classdir = classifier_dir
+		self.classpath = classifier_path
 		#Set up data files to be iterate over, filter for only channel 1 .tiffs
 		for self.root, self.directories, self.filenames in os.walk(source_dir):
 			self.filenames.sort();
-			for filename in self.filenames:
-				# Check for file extension
-				if not filename.endswith('.tif') or not 'C0001' in filename:
+			for filename in self.filenames: # Check for file extension
+				if not filename.endswith('.tif') or 'C0001' not in self.filenames:
 					self.filenames.remove(filename)
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
+			print "Created new directory {}".format(output_dir)
+#		print len(self.filepaths)
 		return None
-		
-	def run(self):
-		for self.root, self.directories, self.filenames in os.walk(self.srcdir):
-			self.filenames.sort();
-			for filename in self.filenames:
-				# Check for file extension
-				if not filename.endswith('.tif') or not 'C0001' in filename:
-					continue
-				process(srcDir, dstDir, root, filename, classifier_Dir)
-			
-	def process(self, srcDir, dstDir, currentDir, fileName, classifier_Dir):
-		print "Processing:"
-		
-		# Opening the image
-		print "Open image file", fileName
-		image = IJ.openImage(os.path.join(srcDir, fileName))
-		
-		weka = WekaSegmentation()
-		weka.setTrainingImage(image)
-		
-		# loads manually trained classifier
-		weka.loadClassifier(classifier_Dir)
-		# apply classifier and get results
-		segmented_image = weka.applyClassifier(image, 0, False)
+	
+	def run(self,threadcount=None):
+		'''Function run allows function process to be run in parallel with varying number of threads.'''
+		pool = Executors.newFixedThreadPool(threadcount) # Define Threads
+		process = [process(filename=image) for image in self.filenames] # define the task to do in a multithreaded way
+		pool.invokeAll(process) # use all defined threads to process the images
+		self.shutdown_and_await_termination(pool=pool, timeout=5)
+		return None
+	
+	def process(self, filename=None):
+		'''Function perfoming the segmentation of a phase contrast image with trained weka function.'''
+		image = IJ.openImage(os.path.join(self.srcdir,filename))
+		weka = WekaSegmentation() # Create weka instance.
+		weka.setTrainingImage(image) # Set image to test (Not in init)
+		weka.loadClassifier(self.classpath) # loads manually trained classifier
+		segmented_image = weka.applyClassifier(image, 0, False) # apply classifier and get results. (Not in init)
 		# assign same LUT as in GUI. Within WEKA GUI, right-click on classified image and use Command Finder to save the "LUT" within Fiji.app\luts
 		#lut = LutLoader.openLut(r'C:\Users\angu312\Documents\Fiji.app\luts\Classification result.lut')
 		#segmented_image.getProcessor().setLut(lut)
-		
-		# Saving the image as a .tif file
-		saveDir = dstDir
-		if not os.path.exists(saveDir):
-			os.makedirs(saveDir)
-		print "Saving to", saveDir
-		IJ.saveAs(segmented_image, "Tif", os.path.join(saveDir, fileName.replace('.tif', '') + '_weka'))
-		image.close()
+		# Save the image as a .tif file
+		IJ.saveAs(segmented_image, "Tif", os.path.join(self.outdir, filename.replace('.tif', '') + '_weka'))
+		image.close()
+		return None
+
+	def shutdown_and_await_termination(self,pool=None, timeout=None):
+		'''Function for shutting down the pool taken from: http://www.jython.org/jythonbook/en/1.0/Concurrency.html'''
+		pool.shutdown()
+		try:
+			if not pool.awaitTermination(timeout, TimeUnit.SECONDS):
+				pool.shutdownNow()
+			if (not pool.awaitTermination(timeout, TimeUnit.SECONDS)):
+				print >> sys.stderr, "Pool did not terminate"
+		except InterruptedException, ex:
+			# (Re-)Cancel if current thread also interrupted
+			pool.shutdownNow()
+			# Preserve interrupt status
+			Thread.currentThread().interrupt()
+		return None
